7aecbf9adc1e080a577b6196178a2a89be85822f
prosenjeetshil <prosenjeetshil@gmail.com>
2023-02-02 20:58:12 +0530

    Questions reducer for handling questions actions

---
+++ src/reducers/questions.js
@@ -0,0 +1,35 @@
+import {
+  RECEIVE_QUESTIONS,
+  SAVE_VOTE,
+  ADD_QUESTION,
+} from "../actions/questions";
+
+const questions = (state = {}, action) => {
+  switch (action.type) {
+    case RECEIVE_QUESTIONS:
+      return {
+        ...state,
+        ...action.questions,
+      };
+    case SAVE_VOTE:
+      return {
+        ...state,
+        [action.qid]: {
+          ...state[action.qid],
+          [action.answer]: {
+            ...state[action.qid][action.answer],
+            votes: [...state[action.qid][action.answer].votes, action.authedUser],
+          },
+        },
+      };
+    case ADD_QUESTION:
+      return {
+        ...state,
+        [action.question.id]: action.question,
+      };
+    default:
+      return state;
+  }
+};
+
+export default questions;
